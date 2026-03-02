f60be28de3d1d64dfa36639a6cdb142d7d8d38cf
Kenneth Ao <kao@csumb.edu>
2026-03-01 18:50:53 -0800

    everything done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Happy developing ✨')
+const form = document.querySelector('form');
+const email = document.getElementById("email");
+const emailError = document.querySelector('span.error');
+
+email.addEventListener("input", (event) => {
+    if (email.validity.valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+    } else {
+        showError();
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    if (!email.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+})
+
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = "Please enter an email";
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = "Entered value needs to be an email address";
+    } else if (email.validity.tooShort) {
+        emailError.textContent = `Email should be at least ${email.minLength} characters`;
+    }
+
+    emailError.className = "error active";
+}
